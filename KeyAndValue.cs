ddde797496092974c6f6290646a08fde3a72fb58
jagjitphull <jagjit.phull@gmail.com>
2024-02-20 05:17:17 +0530

    Create KeyAndValue.cs

---
+++ KeyAndValue.cs
@@ -0,0 +1,9 @@
+//Create the Data dir and create this file
+
+namespace RedisAndEntityFrameworkInWebApi.Data;
+
+public class KeyAndValue
+{
+        public string Key { get; set; }
+        public string Value { get; set; }
+}
