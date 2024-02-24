bb07e86c28d923715c797103332bd3a530ebcfa8
jagjitphull <jagjit.phull@gmail.com>
2024-02-24 09:49:47 +0530

    Delete KeyAndValue.cs

--- KeyAndValue.cs
+++
@@ -1,9 +0,0 @@
-//Create the Data dir and create this file
-
-namespace RedisAndEntityFrameworkInWebApi.Data;
-
-public class KeyAndValue
-{
-        public string Key { get; set; }
-        public string Value { get; set; }
-}
